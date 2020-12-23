084de360daff450f61316bb7199a68f2e71a6c51
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2020-12-24 02:42:06 +0600

    reverted back to broken version.

--- dist/index.js
+++ dist/index.js
@@ -68957,7 +68957,7 @@ const {promisify} = __webpack_require__(31669);
 const fs = __webpack_require__(35747);
 const path = __webpack_require__(85622);
 const fastGlob = __webpack_require__(43664);
-const gitIgnore = __webpack_require__(91230);
+const gitIgnore = __webpack_require__(12069);
 const slash = __webpack_require__(97543);
 
 const DEFAULT_IGNORE = [
@@ -69259,61 +69259,7 @@ module.exports.gitignore = gitignore;
 
 /***/ }),
 
-/***/ 32408:
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-"use strict";
-
-const {Transform} = __webpack_require__(92413);
-
-class ObjectTransform extends Transform {
-	constructor() {
-		super({
-			objectMode: true
-		});
-	}
-}
-
-class FilterStream extends ObjectTransform {
-	constructor(filter) {
-		super();
-		this._filter = filter;
-	}
-
-	_transform(data, encoding, callback) {
-		if (this._filter(data)) {
-			this.push(data);
-		}
-
-		callback();
-	}
-}
-
-class UniqueStream extends ObjectTransform {
-	constructor() {
-		super();
-		this._pushed = new Set();
-	}
-
-	_transform(data, encoding, callback) {
-		if (!this._pushed.has(data)) {
-			this.push(data);
-			this._pushed.add(data);
-		}
-
-		callback();
-	}
-}
-
-module.exports = {
-	FilterStream,
-	UniqueStream
-};
-
-
-/***/ }),
-
-/***/ 91230:
+/***/ 12069:
 /***/ ((module) => {
 
 // A simple implementation of make-array
@@ -69913,6 +69859,60 @@ if (
     REGIX_IS_WINDOWS_PATH_ABSOLUTE.test(path)
     || isNotRelative(path)
 }
+
+
+/***/ }),
+
+/***/ 32408:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+
+const {Transform} = __webpack_require__(92413);
+
+class ObjectTransform extends Transform {
+	constructor() {
+		super({
+			objectMode: true
+		});
+	}
+}
+
+class FilterStream extends ObjectTransform {
+	constructor(filter) {
+		super();
+		this._filter = filter;
+	}
+
+	_transform(data, encoding, callback) {
+		if (this._filter(data)) {
+			this.push(data);
+		}
+
+		callback();
+	}
+}
+
+class UniqueStream extends ObjectTransform {
+	constructor() {
+		super();
+		this._pushed = new Set();
+	}
+
+	_transform(data, encoding, callback) {
+		if (!this._pushed.has(data)) {
+			this.push(data);
+			this._pushed.add(data);
+		}
+
+		callback();
+	}
+}
+
+module.exports = {
+	FilterStream,
+	UniqueStream
+};
 
 
 /***/ }),
